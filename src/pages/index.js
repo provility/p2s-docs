660c53334588c64701aebed2a0aa9a89ab64c9c2
umarfariz1266 <fariz@provility.com>
2026-03-16 15:28:17 +0530

    modified the path according to the point2space domain

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home() {
-  return <Redirect to="/p2s-docs/expressions/geometry/point" />;
+  return <Redirect to="/expressions/geometry/point" />;
 }
